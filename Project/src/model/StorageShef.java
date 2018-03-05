f129ced22a5618482624e2ae811cfdddc76b1fd0
Miraj <miraj.shah@hotmail.co.uk>
2018-03-05 15:02:38 +0000

    storageshelf

---
+++ Project/src/model/StorageShef.java
@@ -0,0 +1,23 @@
+package model;
+
+public class StorageShef implements Entity{
+
+	@Override
+	public void generateID() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Robot compare(Robot r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
