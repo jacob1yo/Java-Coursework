c2d66bf4b147c9b4f7aa7ed107d9061a355701b1
Hamza Zaman <36890317+zamanmh1@users.noreply.github.com>
2018-03-05 15:02:24 +0000

    Robot class
    Added in the empty methods.

--- Project/src/model/Robot.java
+++ Project/src/model/Robot.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Robot {
-
+	private int safetyMargin, batteryLevel;
+	private boolean orderStatus;
+	
+	public Robot() {
+		safetyMargin = 20;
+		batteryLevel = 100;
+		orderStatus = false;
+	}
+	
+	public int getBatteryLevel() {
+		return batteryLevel;
+	}
+	
+	public void decreaseBatteryLevel() {
+		
+	}
+	
+	public void processOrder() {
+		
+	}
+	
+	public boolean needsCharging() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void pickUpOrder() {
+		
+	}
+	
+	public void dropOrder() {
+		
+	}
 }
