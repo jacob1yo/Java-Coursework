a6eeb22e9c03129699591d3ac3e52f35144eb355
Miraj <miraj.shah@hotmail.co.uk>
2018-03-05 14:55:00 +0000

    Entity inteface update

--- Project/src/model/Entity.java
+++ Project/src/model/Entity.java
@@ -2,4 +2,8 @@ package model;
 
 public interface Entity {
 
+	public abstract void generateID();
+	public abstract String getID();
+	public abstract Robot compare(Robot r);
+	//public abstract Location getLocation();	//do we need a location class?
 }
