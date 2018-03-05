2f0a51bede62a1260f4d9d7d616eeef8b2f9eae3
cmdgr <cdolopikos.cd@gmail.com>
2018-03-05 15:13:38 +0000

    Updated 0.1
    Contains methods unfilled and the name of the initial author!

--- Project/src/model/PackingStation.java
+++ Project/src/model/PackingStation.java
@@ -1,9 +1,27 @@
 package model;
+/**
+ * 
+ * @author Christos Dolopikos 
+ *
+ */
 
 public class PackingStation {
 	//Declare fields
 	public PackingStation() {
 		}
+		private boolean completed () {
+	}
+		private void reset() {
+			
+		}
+		private boolean occupied() {}
+		
+		/*private ORDER nextOrder() {
+			//
+		}*/
+		private void packing() {}
+		
+		private void dispatch() {}
 	}
 
 
