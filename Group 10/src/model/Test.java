1516b72efa7cc66a02e45a52e97edb46ea999aab
jjwill <jjwill@hotmail.co.uk>
2018-03-05 14:00:07 +0000

    Another test xxx
    lets lssefefsfe

--- Group 10/src/model/Test.java
+++ Group 10/src/model/Test.java
@@ -1,39 +1,8 @@
 package model;
 
 public class Test {
-
-<<<<<<< HEAD
-	public int Test() {
-=======
+	
 	public Test() {
-<<<<<<< HEAD
->>>>>>> 5b33f22f16b8cd8a48393f189cb85c7ac846bd48
-=======
-<<<<<<< HEAD
-		Systesdbkjdn
-		Jacob line 1
-		Jacob line 2
-=======
->>>>>>> e2950b5b4831672c9d026449cf59228596e882f1
-		
->>>>>>> 5b33f22f16b8cd8a48393f189cb85c7ac846bd48
-		// TODO Auto-generated constructor stub
-		public int cockSucker(int x, int y)
-		{
-			return x + y;
-		}
-		
-	}
-	
-	public void helloWord() {
-		System.out.print("Hello WOrld");
-	}
-	
-	public String hisNameIsJacob()
-	{
-		return "Alex";
-	}
-	}
-	
 
+	}
 }
