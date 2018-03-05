55f83727a308db75a1f612407ef6815b54dbf5ec
Hamza Zaman <36890317+zamanmh1@users.noreply.github.com>
2018-03-05 15:22:40 +0000

    Changes
    Made changes to PS

--- Project/src/model/PackingStation.java
+++ Project/src/model/PackingStation.java
@@ -1,31 +1,31 @@
 package model;
-<<<<<<< HEAD
 
-=======
 /**
- * 
- * @author @author Miraj Shah, Devin Shingadia, Jacob Williams, Mohammed Hamza Zaman, Vivek Bhukhan, Christos Dolopikos 
- *
+ * @author Miraj Shah, Devin Shingadia, Jacob Williams, Mohammed Hamza Zaman,
+ *         Vivek Bhukhan, Christos Dolopikos
  */
->>>>>>> f41dd30e40ae3f89178661fd951b0dd265d8b321
 
 public class PackingStation {
-	//Declare fields
+	// Declare fields
 	public PackingStation() {
-		}
-		private boolean completed () {
-	}
-		private void reset() {
-			
-		}
-		private boolean occupied() {}
-		
-		/*private ORDER nextOrder() {
-			//
-		}*/
-		private void packing() {}
-		
-		private void dispatch() {}
 	}
 
+	private boolean completed() {
+	}
 
+	private void reset() {
+
+	}
+
+	private boolean occupied() {
+	}
+
+	/*
+	 * private ORDER nextOrder() { // }
+	 */
+	private void packing() {
+	}
+
+	private void dispatch() {
+	}
+}
