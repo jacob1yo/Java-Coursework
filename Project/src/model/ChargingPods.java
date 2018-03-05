417dc27bafd8ed6827a38fbfc643d1f523bae63a
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2018-03-05 15:06:56 +0000

    ChargingPods

--- Project/src/model/ChargingPods.java
+++ Project/src/model/ChargingPods.java
@@ -14,4 +14,22 @@ public class ChargingPods implements Entity {
 		
 	}
 
+	@Override
+	public void generateID() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Robot compare(Robot r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
