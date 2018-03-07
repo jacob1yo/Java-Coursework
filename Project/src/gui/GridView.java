35f6892543c459adb7b41191e91aeeedfe795c69
jjwill <jjwill@hotmail.co.uk>
2018-03-07 15:41:35 +0000

    GUI Documentation

--- Project/src/gui/GridView.java
+++ Project/src/gui/GridView.java
@@ -13,51 +13,79 @@ import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class GridView extends Application{
-	//private static final int BOARD_DIM = 8;
-	private static final int SQUARES = 64;
-	//private Cell[][] boardG = new Cell[8][8];
-	private GridPane board = new GridPane();
 	
+	/**
+	 * Declares a flexible grid called "grid".
+	 * @see #start, #buildGrid, #putSquaresOnGrid
+	 */
+	private GridPane grid = new GridPane();
+	
+	/**
+	 * Creates a Rectangle object.
+	 * @see #putSquaresOnGrid
+	 */
 	Rectangle rect;
 
+	/**
+	 * Initialises "Application" from JavaFX.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	/**
+	 * Calls methods to create the grid, creates the scene, 
+	 * sets the title of the window, sets the padding for the grid.
+	 */
 	public void start(Stage primaryStage){
-		buildBoard(board, 8); //Alter 8 to users input
-		putSquaresOnBoard(board, 8); //Ditto
-		board.setPadding(new Insets(15,15,15,15));
-		Scene scene = new Scene(board, 700, 700);
+		buildGrid(grid, 8); //Alter 8 to users input
+		putSquaresOnGrid(grid, 8); //Ditto
+		grid.setPadding(new Insets(15,15,15,15));
+		Scene scene = new Scene(grid, 700, 700);
 		primaryStage.setTitle("Grid");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
-	private void buildBoard(GridPane board, int boardDim){
-		for(int i = 0; i < boardDim; i++){
+	/**
+	 * Determines the number of squares in the grid, dimensions, 
+	 * and the overall layout of the grid (not showing anything)
+	 * @param grid
+	 * @param gridDim
+	 */
+	private void buildGrid(GridPane grid, int gridDim){
+		int squares = gridDim * gridDim;
+		for(int i = 0; i < gridDim; i++){
 			RowConstraints rc = new RowConstraints();
-			rc.setMinHeight(SQUARES);
-			rc.setMaxHeight(SQUARES);
-			rc.setPrefHeight(SQUARES);
+			rc.setMinHeight(squares);
+			rc.setMaxHeight(squares);
+			rc.setPrefHeight(squares);
 			rc.setValignment(VPos.CENTER);
-			board.getRowConstraints().add(rc);
+			grid.getRowConstraints().add(rc);
 
 			ColumnConstraints cc = new ColumnConstraints();
-			cc.setMinWidth(SQUARES);
-			cc.setMaxWidth(SQUARES);
-			cc.setPrefWidth(SQUARES);
+			cc.setMinWidth(squares);
+			cc.setMaxWidth(squares);
+			cc.setPrefWidth(squares);
 			cc.setHalignment(HPos.CENTER);
-			board.getColumnConstraints().add(cc);
+			grid.getColumnConstraints().add(cc);
 		}
 	}
-	private void putSquaresOnBoard(GridPane board, int boardDim) {
-		for (int i = 0; i < boardDim; i ++){
-			for (int j = 0; j < boardDim; j++){
-				rect = new Rectangle(SQUARES,SQUARES,Color.TRANSPARENT);
+	
+	/**
+	 * Creates rectangles to display the grid
+	 * @param grid
+	 * @param gridDim
+	 */
+	private void putSquaresOnGrid(GridPane grid, int gridDim) {
+		int squares = gridDim * gridDim;
+		for (int i = 0; i < gridDim; i ++){
+			for (int j = 0; j < gridDim; j++){
+				rect = new Rectangle(squares,squares,Color.TRANSPARENT);
 				rect.setStroke(Color.BLACK);
 				rect.setStrokeWidth(5);
-				board.add(rect,i,j);
+				grid.add(rect,i,j);
 			}
 		}
 	}
