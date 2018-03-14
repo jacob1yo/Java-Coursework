7c1557ad1f9d39dfed67086627b2748018bf98ca
Hamza Zaman <36890317+zamanmh1@users.noreply.github.com>
2018-03-14 15:31:16 +0000

    Adding behaviours

--- Project/src/gui/MainController.java
+++ Project/src/gui/MainController.java
@@ -1,5 +1,19 @@
 package gui;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+	@FXML private Number height, width;
+	
+	public MainController() {
+		((Object) height).valueProperty();
+		
+		
+		
+	}
+	@FXML public void alterRow() {};
+	@FXML public void alterHeight() {};
 
 }
+
+
