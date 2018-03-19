526511ab0eee8db989ba22ff265d69668b344c8b
Miraj <miraj.shah@hotmail.co.uk>
2018-03-19 11:53:24 +0000

    antonio's changes

---
+++ Project/src/gui/Main.java
@@ -0,0 +1,31 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		try {
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("MainScene.fxml"));
+			loader.setController(new MainController());
+			final Parent root = loader.load();
+			final Scene scene = new Scene(root, 400, 300);
+			primaryStage.setTitle("Warehouse simulator");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- Project/src/gui/MainController.java
+++ Project/src/gui/MainController.java
@@ -1,10 +1,30 @@
 package gui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class MainController {
-	@FXML private Number height, width;
+	@FXML private Slider height, width;
+	@FXML private GridPane grid;
 
+	@FXML public void initialize() {
+		height.valueProperty().addListener(new ChangeListener<>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+				final int newValue = arg2.intValue();
+				grid.getChildren().clear();
+				grid.add(new Pane(), 0, newValue);
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
 	
 	public MainController() {
 		
@@ -12,8 +32,10 @@ public class MainController {
 		
 	}
 	// changing the rows and columns of the grid
-	@FXML public void alterRow() {};
-	@FXML public void alterHeight() {};
+	@FXML public void alterRow() {}
+	@FXML public void alterColumn() {}
+	
+	@FXML public void alterHeight() {}
 
 }
 
