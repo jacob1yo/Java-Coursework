a8db55a4b7ccacc19669890fc39046c27db78ac9
Miraj <miraj.shah@hotmail.co.uk>
2018-03-21 15:48:36 +0000

    change if you want...this was a test

--- Project/src/model/Warehouse.java
+++ Project/src/model/Warehouse.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Warehouse {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Warehouse extends Application{
+	
+	public Warehouse() {
+		
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		GridPane grid = new GridPane();
+		Scene scene = new Scene(grid, 400, 400);
+		
+		grid.addColumn(columnIndex, children);
+		grid.addRow(rowIndex, children);
+		
+	}
+	
+	
 
 }
