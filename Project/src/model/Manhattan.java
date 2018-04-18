1a334060bbc03bb1431198388d638dbcb4b95255
cmdgr <cdolopikos.cd@gmail.com>
2018-04-18 17:26:44 +0300

    MAJOR UPDATE

---
+++ Project/src/model/Manhattan.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+import java.awt.Point;
+
+
+/**
+ * Write a description of class Manhattan here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Manhattan extends Robot implements Entity {
+    // instance variables - replace the example below with your own
+	private ArrayList <Point> path;
+    
+    /**
+     * Constructor for objects of class Manhattan
+     */
+    public Manhattan()
+    {
+        // initialise instance variables
+       path= new ArrayList <Point>();    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int manhattancalc ()
+    {
+        // put your code here
+        
+        }
+}
