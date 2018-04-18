708545c54f000982658b7d5dc3515e20149bde1e
Hamza Zaman <36890317+zamanmh1@users.noreply.github.com>
2018-04-18 20:06:23 +0100

    Formatting
    Commented out errors. Will come back to them later.

--- Project/src/model/Manhattan.java
+++ Project/src/model/Manhattan.java
@@ -6,48 +6,49 @@ import gui.MainController;
 
 import java.awt.Point;
 
-
 /**
  * Write a description of class Manhattan here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Miraj Shah, Devin Shingadia, Jacob Williams, Mohammed Hamza Zaman,
+ *         Vivek Bhukhan, Christos Dolopikos
+ * 
  */
 public class Manhattan extends Robot implements Entity {
-	// instance variables - replace the example below with your own
-	private ArrayList <Point> freeSpaces;
+	private ArrayList<Point> freeSpaces;
 	private ArrayList<Point> robotLocations;
-	private double robotY= getRobotY();
-	private double robotX= getRobotX();
-	private double storageY = super.getStorageY();
-	private double storageX = getStorageX();
-	private double packingY = getPackingY();
-	private double packingX = getPackingX();
+	private double robotY = getRobotY();
+	private double robotX = getRobotX();
+
+	/*
+	 * private double storageY = super.getStorageY(); private double storageX =
+	 * getStorageX(); private double packingY = getPackingY(); private double
+	 * packingX = getPackingX();
+	 */
+
 	/**
 	 * Constructor for objects of class Manhattan
 	 */
-	public Manhattan()
-	{
+	public Manhattan() {
 		ArrayList<Point> freeSpaces = freeSpacePoints(MainController.getNumRows(), MainController.getNumCols());
 		ArrayList<Point> robotLocations = super.robotPoints();
 	}
 
-	public void manhattanCalc () {
-		for(int i = 0; i < robotLocations.size(); i++) {
-			for(int j = 0; j < freeSpaces.size(); j++) {
+	public void manhattanCalc() {
+		for (int i = 0; i < robotLocations.size(); i++) {
+			for (int j = 0; j < freeSpaces.size(); j++) {
 				double x = robotLocations.get(i).getX();
 				double y = robotLocations.get(i).getY();
-				if(freeSpaces.get(j).getX() == x - 1 && freeSpaces.get(j).getY() == y) {
-					//(x - 1, y) is available
+				if (freeSpaces.get(j).getX() == x - 1 && freeSpaces.get(j).getY() == y) {
+					// (x - 1, y) is available
 				}
-				if(freeSpaces.get(j).getX() == x && freeSpaces.get(j).getY() == y - 1) {
-					//(x, y - 1) is available
+				if (freeSpaces.get(j).getX() == x && freeSpaces.get(j).getY() == y - 1) {
+					// (x, y - 1) is available
 				}
-				if(freeSpaces.get(j).getX() == x + 1 && freeSpaces.get(j).getY() == y) {
-					//(x + 1, y) is available
+				if (freeSpaces.get(j).getX() == x + 1 && freeSpaces.get(j).getY() == y) {
+					// (x + 1, y) is available
 				}
-				if(freeSpaces.get(j).getX() == x && freeSpaces.get(j).getY() == y + 1) {
-					//(x, y + 1) is available
+				if (freeSpaces.get(j).getX() == x && freeSpaces.get(j).getY() == y + 1) {
+					// (x, y + 1) is available
 				}
 			}
 		}
