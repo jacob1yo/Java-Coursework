8e9eb1fa34d28affb9fca0d3f460e1be6ad7361e
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2018-04-20 10:52:14 +0100

    FillLists method updates

--- Project/src/model/Order.java
+++ Project/src/model/Order.java
@@ -5,101 +5,76 @@ package model;
  *
  */
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Order {
-	
+
 	/**
 	 * Contains the orders
-	 * @see #show, #isCompleted, #addtoUnassigned, #addToAssigned, #removeFromUnassigned, #removeFromAssigned
+	 * 
+	 * @see #show, #isCompleted, #addtoUnassigned, #addToAssigned,
+	 *      #removeFromUnassigned, #removeFromAssigned
 	 */
 	private static ArrayList<String> commands = new ArrayList<String>(); // reads a full sim file
-	private static ArrayList<String> orders = new ArrayList<String>(); 
+	private static ArrayList<String> orders = new ArrayList<String>();
 	private static ArrayList<String> configuration = new ArrayList<String>();
 	private static ArrayList<String> podRob = new ArrayList<String>();
 	private static ArrayList<String> shelves = new ArrayList<String>();
 	private static ArrayList<String> stations = new ArrayList<String>();
 	private static File file;
-	
+
 	/**
 	 * Reads the orders from a file.
 	 */
 
-		
 	public Order() {
 	}
+
+	/*
+	 * public static ArrayList<Point> getCoordinates() { String[] coordinates =
+	 * processData(); for (int i = 0; i < coordinates.length; i+=4) { for (int j =
+	 * 0; j < coordinates.length; j+)
+	 * 
+	 * } 
+	 * }
+	 */
 	
-	/*public static void processData() {
-		//int width = fileWidth;
-		//int height = fileHeight;
-		// this.coordinates = coordinates;
-		
+	/**
+	 * Reads the .sim file and stores each line in an ArrayList
+	 * 
+	 * @return
+	 */
+	public static ArrayList<String> printCommands() {
 		try {
 			Scanner scanner = new Scanner(file);
 			while (scanner.hasNextLine()) {
-				if (scanner.equals("podRob")) {
-					podRob = scanner.nextLine().split(" ");
-				}
-				data = scanner.nextLine().split(" ");						
-				for (int i = 0; i < data.length; i++) {
-					System.out.println(data[i].toString());
-				}
-				
-				for (int i = 0; i < podRob.length; i++) {
-					System.out.println(podRob.length);
-				}
-				
-			}
-			scanner.close();
-			
-		} catch (FileNotFoundException e) {
-	}
-	}*/
-	
-	/*
-	public static ArrayList<Point> getCoordinates() {
-		String[] coordinates = processData();
-		for (int i = 0; i < coordinates.length; i+=4) {
-			for (int j = 0; j < coordinates.length; j+)
-			
-		}
-	}
-*/
-	/**
-	 * Reads the .sim file and stores each line in an ArrayList
-	 * @return
-	 */
-	public static ArrayList<String> printCommands(){
-		try{
-			Scanner scanner = new Scanner(file);
-			while(scanner.hasNextLine()) {
 				String line = scanner.nextLine();
-				commands.add(line);	
+				commands.add(line);
 			}
-			
+
 			scanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return commands;
 	}
-	
+
 	/**
 	 * Prints the .sim file in the GUI
+	 * 
 	 * @return
 	 */
-	public static String getCommands(){
+	public static String getCommands() {
 		String line = "";
-		for(int i=0; i < commands.size(); i++ ) {
+		for (int i = 0; i < commands.size(); i++) {
 			line += commands.get(i) + "\n";
 		}
 		return line;
 	}
-	
+
 	/**
 	 * Filters the results from the .sim file into their respective ArrayLists
 	 */
@@ -109,6 +84,7 @@ public class Order {
 			clearLists();
 			while(scanner.hasNextLine()) {
 				String line = scanner.nextLine();
+			
 				if(line.contains("podRobot")) {
 					String[] temp = line.split(" ");
 					for (int i = 0; i < temp.length;i++) {
@@ -117,30 +93,43 @@ public class Order {
 				
 				}
 				else if(line.contains("shelf")) {
-					shelves.add(line);
+					String[] temp = line.split(" ");
+					for (int i = 0; i < temp.length;i++) {
+						shelves.add(temp[i]);
+						}
+					
 				}
 				else if(line.contains("station")) {
-					stations.add(line);
+					String[] temp = line.split(" ");
+					for (int i = 0; i < temp.length;i++) {
+					stations.add(temp[i]);
+					}
 				}
 				else if(line.contains("order")){
-					orders.add(line);
+					String[] temp = line.split(" ");
+					for (int i = 0; i < temp.length;i++) {
+						orders.add(temp[i]);
+					}
 				}
 				else {
-					configuration.add(line);
+					String[] temp = line.split(" ");
+					for (int i = 0; i < temp.length;i++) {
+						configuration.add(temp[i]);
 				}
 			}
 			scanner.close();
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		System.out.println(podRob.size());
-		
+			
 	}
-	
+
 	/**
 	 * Clears all the ArrayLists
 	 */
-	public static void clearLists() { 
+	public static void clearLists() {
 		podRob.clear();
 		shelves.clear();
 		stations.clear();
@@ -148,37 +137,41 @@ public class Order {
 		orders.clear();
 		configuration.clear();
 	}
-	
+
 	public static void setFile(File f) {
 		// happens in maincontroller just sets the file that's been chosen
 		file = f;
 	}
 
-	
 	/**
-	 * Checks if an order has been completed, and moves it to
-	 * "completedOrders" if it is completed.
+	 * Checks if an order has been completed, and moves it to "completedOrders" if
+	 * it is completed.
 	 */
-	public void isCompleted() {}
-	
+	public void isCompleted() {
+	}
+
 	/**
 	 * Adds an order to the "unassigned" list.
 	 */
-	public void addToUnassigned() {}
-	
+	public void addToUnassigned() {
+	}
+
 	/**
 	 * Adds an order to the "assigned" list.
 	 */
-	public void addToAssigned() {}
-	
+	public void addToAssigned() {
+	}
+
 	/**
 	 * Removes an order from the "unassigned" list.
 	 */
-	public void removeFromUnassigned() {}
-	
+	public void removeFromUnassigned() {
+	}
+
 	/**
 	 * Removes an order from the "assigned" list.
 	 */
-	public void removeFromAssigned() {}
-	
+	public void removeFromAssigned() {
+	}
+
 }
