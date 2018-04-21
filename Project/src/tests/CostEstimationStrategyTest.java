6f90991e69ed1873dc92be303b3c07e010493d2b
Devin S <36890301+shingad1@users.noreply.github.com>
2018-04-21 21:50:19 +0100

    adding cost estimation test

---
+++ Project/src/tests/CostEstimationStrategyTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CostEstimationStrategyTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCostEstimationStrategy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDistanceCalculator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDistanceToSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTotalDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPointToPoint() {
+		fail("Not yet implemented");
+	}
+
+}
