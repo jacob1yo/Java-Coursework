4fb447227857dc6f65d519b52e3a277ec6f95ce3
Miraj <miraj.shah@hotmail.co.uk>
2018-04-22 15:04:13 +0100

    change in generateId

--- Project/src/model/PackingStation.java
+++ Project/src/model/PackingStation.java
@@ -123,11 +123,8 @@ public class PackingStation extends Warehouse implements Entity{
 	
 	@Override
 	public void generateID(int id) {
-		if(uid == null) {
-			//int num = lastNum++;
-			int num = id;
-			uid = "ps" + num;
-		}
+		int num = id;
+		uid = "ps" + num;
 	}
 
 	@Override
