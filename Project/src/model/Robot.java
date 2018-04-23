02ca064b10a2fda24169ccd2b6e7d7555f290166
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2018-04-23 10:29:55 +0100

    Robot class documentation completed but method implementations missing

--- Project/src/model/Robot.java
+++ Project/src/model/Robot.java
@@ -19,42 +19,59 @@ public class Robot extends Warehouse implements Entity  {
 	/**
 	 * The value of the battery level of Robot objects.
 	 * 
-	 * @see #orderDecision #getBatteryLevel #decreaseBatteryLevel
+	 * @see #orderDecision #getBatteryLevel #decreaseBatteryLevel #updateBattery #orderDecision
 	 */
 	private int batteryLevel;
 	
-	
+	/**
+	 * The value of the safety margin for each robots battery level
+	 * 
+	 * @see #orderDecision
+	 */	
 	private double safetyMargin;
 
 	/**
 	 * Whether the robot is currently processing an order or not
 	 * 
-	 * @see #processOrder
+	 * @see #decreaseBatteryLevel #orderDecision #pickUpItems #dropOrder
 	 */
 	private boolean orderStatus;
+	
 	/**
 	 * The unique identifier of each robot
 	 * 
-	 * @see #getID, #generateID
+	 * @see #generateID, #setId
 	 */
 	private String uid;
+	
 	/**
 	 * Stores the last number used for the ID
+	 * 
+	 * @see #resetID
 	 */
 	private static int lastNum = 0;
-
+	
+	/**
+	 * The Point coordinates where the Robot is placed on the grid.
+	 * 
+	 * @see #setCoordinates #getRobotCoordinates #getRobotX #getRobotY
+	 */
 	protected static Point robotCoordinates;
-
-	//add fields, explain how this might change
-
+	
+	/**
+	 * Storage Shelf Constructor. Creates a <code>Robot</code> and sets a safety margin of 20%. It also has a <code>boolean</code>
+	 * set to false automatically for the Order Status.
+	 * 
+	 */
 	public Robot() {
 		safetyMargin = 0.2;
 		orderStatus = false;
 	}
+	
 	/**
 	 * The current battery level of the robot.
 	 * 
-	 * @return <code>int</code> how much battery life of a robot is left.
+	 * @return <code>int</code> How much battery life of a robot is left.
 	 *         {@link #batteryLevel}
 	 */
 	public int getBatteryLevel() {
@@ -74,7 +91,9 @@ public class Robot extends Warehouse implements Entity  {
 	}
 
 	/**
+	 * Updates the battery level of the robot.
 	 * 
+	 * @param batteryLevel a {@link batteryLevel} containing value of the battery level for the robot.
 	 */
 	public void updateBattery(int batteryLevel) {
 		this.batteryLevel = batteryLevel;
@@ -82,7 +101,7 @@ public class Robot extends Warehouse implements Entity  {
 
 	/**
 	 * Decides whether a robot can take an order or not.
-	 * @return <code>boolean</code> true if an order is accepted, otherwise false.
+	 * @return <code>boolean</code> True if an order is accepted, otherwise false.
 	 */
 	public boolean  orderDecision() {
 		if (orderStatus == false) {
@@ -114,6 +133,9 @@ public class Robot extends Warehouse implements Entity  {
 
 	/**
 	 * Robots takes the items from the storage shelf.
+	 * 
+	 * @param r a Robot object.
+	 * @param ss a Storage Shelf object.
 	 */
 	public void pickUpItems(Robot r, StorageShelf ss) {
 
@@ -126,37 +148,46 @@ public class Robot extends Warehouse implements Entity  {
 	}
 
 	/**
-	 * When a robot drops off items at a packing station and waits until it's
-	 * packed.
+	 * When a robot drops off items at a packing station and waits until it's packed.
+	 * 
+	 * @param r a Robot object.
+	 * @param ps a Packing Station object.
 	 */
 	public void dropOrder(Robot r, PackingStation ps) {
 		//some code here
 		orderStatus = true;
 	}
-
+	
+	/**
+	 * Sets the coordinate values for the robot and uses the X and Y int values to create a point on the grid.
+	 * 
+	 * @param x a int value. Used for the X coordinate.
+	 * @param y a int value. Used for the Y coordinate.
+	 */
 	public void setCoordinates(int x, int y) {
 		robotCoordinates = new Point(x, y);
 	}
 
 	/**
-	 * Gets the X n' Y co-ordinates of the robot.
-	 * @return <code>Point</code> The co-ordinate value.
+	 * Gets the X and Y coordinates of the robot.
+	 * 
+	 * @return <code>Point</code> The coordinate value.
 	 */
 	public Point getRobotCoordinates() {
 		return robotCoordinates;
 	}
 
 	/**
-	 * Gets the Y co-ordinate of the robot.
-	 * @return <code>int</code> The co-ordinate value.
+	 * Gets the Y coordinate of the robot.
+	 * @return <code>double</code> The Y coordinate value.
 	 */
 	public static double getRobotY() {
 		return robotCoordinates.getY();
 	}
 
 	/**
-	 * Gets the x co-ordinate of the robot.
-	 * @return <code>int</code> The co-ordinate value.
+	 * Gets the X coordinate of the robot.
+	 * @return <code>double</code> The X coordinate value.
 	 */
 	public static double getRobotX() {
 		return robotCoordinates.getX();
@@ -169,11 +200,24 @@ public class Robot extends Warehouse implements Entity  {
 		lastNum = 0;
 	}
 
+	/**
+	 * Gets the location of each robot from the {@link MainController class} and stores it in an ArrayList. 
+	 * 
+	 * @return Returns a <code>ArrayList<Point></code>. ArrayList of Point Objects.
+	 */
 	public ArrayList<Point> getRobotSpaces() {
 		return MainController.getRobotSpaces();
 
 	}
 
+	/**
+	 * Gets the free spaces on the of free spaces on the grid {@link MainController class} and stores it in an ArrayList. 
+	 * 
+	 * @param numCols an int value representing the number of columns in the current grid.
+	 * @param numRows an int value representing the number of rows in the current grid.
+	 * 
+	 * @return Returns a <code>ArrayList<Point></code>. ArrayList of Point Objects.
+	 */
 	public ArrayList<Point> getFreeSpacePoints(int numCols, int numRows) {
 		return MainController.getFreeSpaces(numCols, numRows);
 	}
