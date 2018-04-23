d89996f6094df32faa3d582f862459b155d1de5b
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2018-04-23 09:32:52 +0100

    Documentation changes

--- Project/src/model/Entity.java
+++ Project/src/model/Entity.java
@@ -12,13 +12,15 @@ package model;
 public interface Entity {
 	
 	/** 
-	 * Generates the ID for the each ChargingPod object
+	 * Generates the ID for the each ChargingPod object.
+	 * 
 	 * @param id. ID value assigned to num and then incremented by 1 for each object created
 	 */
 	public abstract void generateID(int id);
 	
 	/**
-	 * Gets the ID generated 
+	 * Gets the ID generated.
+	 * 
 	 * @return <code>String</code>. The uid of the Charging Pod object created.
 	 */
 	public abstract String getID();
@@ -31,7 +33,9 @@ public interface Entity {
 	
 	/**
 	 * Compares if the robot is in the same position as another entity.
-	 * @param r a Robot object
+	 * 
+	 * @param r a Robot object.
+	 * 
 	 * @return <code>boolean</code> true if there is an entity in the same location or false otherwise.
 	 */
 	public abstract boolean compare(Entity r);
