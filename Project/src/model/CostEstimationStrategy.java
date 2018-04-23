ee2d5730d6bda478dfe5d98b99bc42b3130f03ed
cmdgr <cdolopikos.cd@gmail.com>
2018-04-23 17:47:35 +0300

    almost there

--- Project/src/model/CostEstimationStrategy.java
+++ Project/src/model/CostEstimationStrategy.java
@@ -23,7 +23,7 @@ public class CostEstimationStrategy extends Robot {
 	 */
 	private static double noSteps;
 	
-//	private static double storagePacking;
+
 	
 	private static double robotStorage;
 	
@@ -44,30 +44,38 @@ public class CostEstimationStrategy extends Robot {
 	 * @return <code>double</code> Distance between locations
 	 * this is repeated code, so we will write down the maths into a single function and call the function in every time with parameters
 	 */
-	public double distanceEstimator() {
+	public boolean distanceEstimator() {
 		//TODO gets in order and point from packing. clean up. decide order. "i" is sentence = first line of order. j is each word
 		Point destination=PackingStation.passOnPoint();
 		
 		ArrayList<String> sentence = PackingStation.getNextOrder();
+		double storageDistances;
+		double storagePacking;
+		double packingPod;
 		
 		robotStorage = Math.sqrt((Math.pow((Order.storagePoints().get(sentence.get(2)).getX()-getRobotX()),2))+Math.pow((Order.storagePoints().get(sentence.get(2)).getY()-getRobotY()), 2));
 		
-				for (int j=2; j< sentence.size(); j++) {  // gets the ss1 ss2 etc
-
-			double storageDistances = Math.sqrt((Math.pow((Order.storagePoints().get(j).getX()-Order.storagePoints().get(j++).getX()),2))+Math.pow((Order.storagePoints().get(j).getY()-Order.storagePoints().get(j++).getY()),2));
-			
-			double storagePacking = Math.sqrt((Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getX()-destination.getX()),2)) + Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getY()-destination.getY()),2));
-			
-			doublediagonals += storageDistances+storagePacking;
+			for (int j=2; j< sentence.size(); j++) {  // gets the ss1 ss2 etc	
+					if (j++<sentence.size()) {
+						storageDistances = Math.sqrt((Math.pow((Order.storagePoints().get(j).getX()-Order.storagePoints().get(j++).getX()),2))+Math.pow((Order.storagePoints().get(j).getY()-Order.storagePoints().get(j++).getY()),2));
+						}
+					else {
+					 storagePacking = Math.sqrt((Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getX()-destination.getX()),2)) + Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getY()-destination.getY()),2));
+						}
+					doublediagonals += storageDistances+storagePacking;	
 		}
-		noSteps = ((1.422*robotStorage)-0.07577)+(2*(1.422*(doblediagonals)-0.07577));
-		System.out.println(noSteps);
-		return noSteps;
-	}
-	public  boolean decideOrder() {
-		if(distanceEstimator() < (safetyMargin*batteryLevel)+(batteryLevel)) {
-		}
+		packingPod = Math.sqrt((Math.pow((Order.chargePoints().get(2).getX() -destination.getX()),2)) + Math.pow((Order.chargePoints().get(2).getY()-destination.getY()),2));
 		
-		return true;		
+		noSteps = ((1.422*(robotStorage + packingPod))-0.07577)+(2*(1.422*(doublediagonals)-0.07577));
+		System.out.println(noSteps);
+	//	return noSteps;
+	
+
+		if(noSteps < (safetyMargin*batteryLevel)+(batteryLevel)) {
+			return true;
+		}
+		else {
+		return false;		
 	}
+}
 }
