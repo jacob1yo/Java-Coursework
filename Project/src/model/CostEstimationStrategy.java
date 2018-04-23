497d6e5271ac637e906be5f27e14aeef08828973
cmdgr <cdolopikos.cd@gmail.com>
2018-04-23 12:29:17 +0300

    s

--- Project/src/model/CostEstimationStrategy.java
+++ Project/src/model/CostEstimationStrategy.java
@@ -23,9 +23,11 @@ public class CostEstimationStrategy extends Robot {
 	 */
 	private static double noSteps;
 	
-	private static double storagePacking;
+//	private static double storagePacking;
 	
 	private static double robotStorage;
+	
+	private static double doblediagonals;
 	
 	private static double sum;
 	
@@ -44,17 +46,20 @@ public class CostEstimationStrategy extends Robot {
 	 */
 	
 	public void distanceEstimator(int i, String uid) {
-		robotStorage = Math.sqrt((Math.pow((Order.storagePoints().get(uid).getX()-getRobotX()),2))+Math.pow((Order.storagePoints().get(uid).getY()-getRobotY()), 2));
+		robotStorage = Math.sqrt((Math.pow((Order.storagePoints().get(Order.getOrders().get(i)).getX()-getRobotX()),2))+Math.pow((Order.storagePoints().get(uid).getY()-getRobotY()), 2));
 		
 		for (int j=2; j< Order.getOrders().get(i).size(); j++) {  // gets the ss1 ss2 etc
+
 			double storageDistances = Math.sqrt((Math.pow((Order.storagePoints().get(j).getX()-Order.storagePoints().get(j++).getX()),2))+Math.pow((Order.storagePoints().get(j).getY()-Order.storagePoints().get(j++).getY()),2));
-			sum += storageDistances;
-			storagePacking = Math.sqrt((Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getX()-Order.packingPoints().get(uid).getX()),2)) + Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getY()-Order.packingPoints().get(uid).getY()),2));
+			double storagePacking = Math.sqrt((Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getX()-Order.packingPoints().get(uid).getX()),2)) + Math.pow((Order.storagePoints().get(Order.storagePoints().size()-1).getY()-Order.packingPoints().get(uid).getY()),2));
+			doblediagonals += storageDistances+storagePacking;
 		}
+		//return doblediagonals;
 	}
 	
 	public static double distanceToSteps() {
-		noSteps = ((1.422*robotStorage)-0.07577)+(2*(1.422*(sum+storagePacking)-0.07577));
+	//	distanceEstimator(i, uid);
+		noSteps = ((1.422*robotStorage)-0.07577)+(2*(1.422*(doblediagonals)-0.07577));
 		System.out.println(noSteps);
 		return noSteps;
 	
