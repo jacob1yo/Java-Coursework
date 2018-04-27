16ee5de5bde6e30411a3d188d7e0c7fec348acaa
Miraj <miraj.shah@hotmail.co.uk>
2018-04-27 14:49:08 +0100

    costEst docs

--- Project/src/model/CostEstimationStrategy.java
+++ Project/src/model/CostEstimationStrategy.java
@@ -23,18 +23,52 @@ public class CostEstimationStrategy extends Robot {
 	 */
 	private double noSteps;
 
+	/**
+	 * Used in distanceEstimator, to pass in a Order.
+	 * @see #distanceEstiamator
+	 */
 	private Order order;
 
+	/**
+	 * Used in disanceEstimator, to pass in the Packing Station the Robot has to deliver the items to.
+	 * @see #distanceEstimator
+	 */
 	private PackingStation packingStation;
 
+	/**
+	 * <code>HashMap</code> of the coordinates each Storage Shelf.
+	 * @see #distanceEstimator
+	 */
 	private HashMap<String, Point> storagePoints;
 
+	/**
+	 * Constant Field that calculates the gradient in y=mx+c.
+	 * @see #distanceEstimator
+	 */
 	public final double GRADIENT = 1.422;
-
+	
+	/**
+	 * Constant Field that calculates the intercept in y=mx+c.
+	 * @see #distanceEstimator
+	 */
 	public final double INTERCEPT = 0.07577;
 
+	/**
+	 * <code>ArrayList</code> that holds the single order that need to be assigned, from the "SIM" file.
+	 * @see #distanceEstimator #getSentence
+	 */
 	private ArrayList<String> sentence;
 
+	/**
+	 * CostEstimationStrategy Constructor
+	 * Gets the values from the superClass <code>Robot</code>
+	 * Assignees the the fields: Order, PackingStation
+	 * Populates the StoragePoints <code>HashMap</code>.
+	 * Initialises the value of noSteps.
+	 * @param order, holds the instance of the Order class that contains the orders from the "SIM" file.
+	 * @param packing, hold the instance of the Packing Station that the Robot need to deliver the items to.
+	 * @param storagePoints, <code>HashMap</code> containing the coordinates of the Storage Shelves
+	 */
 	public CostEstimationStrategy(Order order, PackingStation packing, HashMap<String, Point> storagePoints) {
 		super();
 		this.order = order;
@@ -44,10 +78,14 @@ public class CostEstimationStrategy extends Robot {
 	}
 
 	/**
-	 * Calculates the distance from one point to another.
-	 * @param 
-	 * @return <code>double</code> Distance between locations
-	 * this is repeated code, so we will write down the maths into a single function and call the function in every time with parameters
+	 * Calculates whether or not a Robot can carry out an order.
+	 * @param x, holds the current x coordinate of the Robot
+	 * @param y, holds the current y coordinate of the Robot
+	 * @param id, holds the Robots UID
+	 * @param batteryLevel, holds the current battery level of the Robot
+	 * @param robotsCharge, <code>HashMap</code> contains the Robots UID, and their respective Charging Pod
+	 * @param charges, <code>HashMap</code> contains the coordinates of each Charging Pod
+	 * @return <code>boolean</code> Returns true if the Robot can carry out the order, else false
 	 */
 	public boolean distanceEstimator(double x, double y, String id, int batteryLvl, HashMap<String, String> robotsCharge, HashMap<String, Point> charges) {
 		double robotX = x;
@@ -112,6 +150,10 @@ public class CostEstimationStrategy extends Robot {
 		return false;
 	}
 
+	/**
+	 * <code>ArrayList</code> contains the Order that needs to be checked.
+	 * @return Returns <code>ArrayList</code> of type <code>String</code>
+	 */
 	public ArrayList<String> getSentence(){
 		return sentence;
 	}
